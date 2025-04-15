5290d378f09ecdbb85f4a70929596b7355722bd7
Gholamreza Dar <rezadar1378@gmail.com>
2025-04-15 14:34:02 +0330

    renaming done, ai stuff remaining

--- ai_renamer.py
+++ ai_renamer.py
@@ -1,20 +1,83 @@
 import os
 import argparse
+import pathlib
 
-# Get the folder path
-parser = argparse.ArgumentParser(description="Rename files in a folder using AI")
-parser.add_argument("folder", help="Folder containing the files to rename")
-parser.parse_args()
+from colorama import Fore, Style, init
+# This is needed on Windows for colorama to work
+init()
 
-print("Renaming files in", parser.folder)
+def rename_files(old_names, new_names):
+    '''Rename old_names to new_names
+    Both lists must have the same length
+    Both lists contain the full paths to the files (not just the file names)
+    '''
+    for old_name, new_name in zip(old_names, new_names):
+        old_path = pathlib.Path(old_name)
+        new_path = pathlib.Path(new_name)
 
-# Get the list of files in the folder
-files = os.listdir(parser.folder)
+        # Add a number suffix if the target file already exists
+        counter = 1
+        temp_path = new_path
+        while new_path.exists() and new_path.name != old_path.name:
+            new_path = new_path.with_name(f"{temp_path.stem}_{counter}{temp_path.suffix}")
+            counter += 1
 
-# Rename the files using AI
-for file in files:
-    # TODO: Rename the file using AI
-    # TODO: Save the renamed file with the original name
-    print(file, "renamed to", file + "_renamed")
-    pass
+        os.rename(old_path, new_path)
 
+def generate_name_based_on_content(file: pathlib.Path):
+    '''Generate a name based on the content of the file using AI
+    Returns None if the file is not text-based
+    '''
+
+    # try to read the file content
+    content = ""
+    try:
+        with open(file, "r") as f:
+            content = f.read()
+    except:
+        print(f"Could not read file {file}")
+        return None
+    
+    # TODO: Generate a name based on the content
+    return content[:10]
+
+def get_ai_name(file: pathlib.Path):
+    '''Returns the full path to the file with the new name'''
+    parent_folder = file.parent
+    extension = file.suffix
+    name = file.stem
+
+    # In case we can't generate a name, return the original name
+    new_name = name
+
+    # If the file is not text-based, returns None
+    generated_name = generate_name_based_on_content(file)
+    if generated_name is not None:
+        new_name = generated_name
+
+    return f"{parent_folder}/{new_name}{extension}"
+
+if __name__ == "__main__":
+
+    # Get the arguments
+    parser = argparse.ArgumentParser(description="Rename files in a folder using AI")
+    parser.add_argument("folder", help="Folder containing the files to rename")
+    args = parser.parse_args()
+
+    print(">> Generating names for files in", args.folder, "...")
+
+    # Get the list of files in the folder
+    folder = pathlib.Path(args.folder)
+    files = [f for f in folder.iterdir() if f.is_file()]
+
+    # Generate new names using using AI
+    new_names = []
+    for file in files:
+        new_name = get_ai_name(file)
+        new_names.append(new_name)
+        print(f"{Fore.RED}{file.name}{Style.RESET_ALL} -> {Fore.GREEN}{new_name}{Style.RESET_ALL}")
+
+    # Rename the files
+    print(">> Renaming files in", args.folder, "...")
+    rename_files(files, new_names)
+    print(">> Done") 
